c5d2076ecbfb217a70e5b22ac7bc016d8e4b33b4
crxstian <cristian19overdrive@gmail.com>
2025-05-24 17:34:45 -0300

    Resolucion TP Recursividad completa

--- 11 Recursividad/SILES_TP-11.py
+++ 11 Recursividad/SILES_TP-11.py
@@ -32,10 +32,26 @@ for i in range(numero_ej2 + 1):
 
 
 # 3) Crea una función recursiva que calcule la potencia de un número base elevado a un
-# exponente, utilizando la fórmula 𝑛
-# 𝑚 = 𝑛 ∗ 𝑛 ^ (𝑚−1)
+# exponente, utilizando la fórmula 𝑛 ^ 𝑚 = 𝑛 ∗ 𝑛 ^ (𝑚−1)
 # . Prueba esta función en un
 # algoritmo general.
+
+def potencia_recursiva(base,exponente):
+    if exponente == 0:  #cualquier número elevado a 0 es 1
+        return 1
+    elif exponente < 0:  #exponentes negativos
+        return 1 / potencia_recursiva(base, -exponente)
+    elif base == 0:
+        return 0  #0 elevado a cualquier exponente positivo es 0
+    else:
+        return base * potencia_recursiva(base, exponente - 1)
+    
+
+numero_ej3 = int(input("Por ingrese un numero base para calcular su potencia de manera recursiva : "))
+
+numero_ej3_exponente = int(input("Por ingrese el exponente para calcular la potencia : "))
+
+print(f"El valor obtenido de manera recursiva de la potencia del numero base: {numero_ej3} y su exponente: {numero_ej3_exponente} es: {potencia_recursiva(numero_ej3,numero_ej3_exponente)}")
 
 
 # 4) Crear una función recursiva en Python que reciba un número entero positivo en base
@@ -55,6 +71,16 @@ for i in range(numero_ej2 + 1):
 # 1 ÷ 2 = 0 resto: 1
 # Leyendo los restos de abajo hacia arriba: 1 0 1 0 → El resultado binario es "1010".
 
+numero_ej4 = int(input("Por ingrese un numero entero positivo en base decimal : "))
+
+def decimal_a_binario_recursivo(numero):
+    if numero <=1:
+        return [numero]
+    else:
+        return decimal_a_binario_recursivo(numero//2) + [numero%2]
+    
+print(f"El resultado de pasar el numero decimal {numero_ej4} a binario es : {decimal_a_binario_recursivo(numero_ej4)}")
+
 
 # 5) Implementá una función recursiva llamada es_palindromo(palabra) que reciba una
 # cadena de texto sin espacios ni tildes, y devuelva True si es un palíndromo o False si no
@@ -62,6 +88,17 @@ for i in range(numero_ej2 + 1):
 #  Requisitos:
 # La solución debe ser recursiva.
 # No se debe usar [::-1] ni la función reversed().
+
+def es_palindromo(palabra):
+    if len(palabra) <=1:
+        return True
+    if palabra[0] != palabra[-1]:
+        return False
+    return es_palindromo(palabra[1:-1])
+
+palabra_ej5 = str(input("Ingrese una palabra para saber si es palindromo o no: "))
+print(f"Si es palindromo el resultado es True, de lo contrario False. El resultado final es {es_palindromo(palabra_ej5)}")
+
 
 
 # 6) Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un
@@ -75,6 +112,13 @@ for i in range(numero_ej2 + 1):
 # suma_digitos(305) → 8 (3 + 0 + 5)
 
 
+def suma_digitos(numero):
+    if numero == 0:
+        return 0
+    return (numero % 10) + suma_digitos(numero//10)
+numero_ej6 = int(input("Por favor ingrese un numero entero positivo: "))
+print(f"La suma de los digitos que componen el numero que ingreso es: {suma_digitos(numero_ej6)}")
+
 # 7) Un niño está construyendo una pirámide con bloques. En el nivel más bajo coloca n
 # bloques, en el siguiente nivel uno menos (n - 1), y así sucesivamente hasta llegar al
 # último nivel con un solo bloque.
@@ -86,6 +130,14 @@ for i in range(numero_ej2 + 1):
 # contar_bloques(2) → 3 (2 + 1)
 # contar_bloques(4) → 10 (4 + 3 + 2 + 1)
 
+def contar_bloques(n):
+    if n == 0:
+        return 0
+    return n + contar_bloques(n-1)
+
+numero_ej7 = int(input("Por favor ingrese la cantidad de bloques de la base de la piramide para calcular cuantos bloques total se necesitan: "))
+print(f"Para construir una piramide cuya base es la ingresada se necesitan {contar_bloques(numero_ej7)} bloques")
+
 
 # 8) Escribí una función recursiva llamada contar_digito(numero, digito) que reciba un
 # número entero positivo (numero) y un dígito (entre 0 y 9), y devuelva cuántas veces
@@ -94,3 +146,14 @@ for i in range(numero_ej2 + 1):
 # contar_digito(12233421, 2) → 3
 # contar_digito(5555, 5) → 4
 # contar_digito(123456, 7) → 0 
+
+def contar_digito(numero,digito):
+    if numero == 0:
+        return 0
+    return (1 if numero%10 ==digito else 0) + contar_digito(numero // 10,digito)
+
+numero_ej8 = int(input("Por favor ingrese un numero entero positivo: "))
+digito_ej8 = int(input("Por favor ingrese un digito entre 0 y 9: "))
+
+print(f"El digito {digito_ej8} aparece {contar_digito(numero_ej8,digito_ej8)} veces en el numero {numero_ej8}")
+
